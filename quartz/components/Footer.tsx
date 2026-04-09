dfea39538f6cf3b6cc2c042ea7c44c65cb8f8b13
unset <evanpontonnier@gmail.com>
2026-04-09 21:28:28 +0200

    license

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -16,7 +16,7 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
 
         {/* ✅ Creative Commons License Section */}
-        <p>
+        <p style="display: flex; align-items: center; gap: 10px;">
           <a
             rel="license"
             href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
@@ -27,20 +27,23 @@ export default ((opts?: Options) => {
               src="https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png"
             />
           </a>
-          <br />
-          This site is licensed under a{" "}
-          <a
-            rel="license"
-            href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
-          >
-            Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
-          </a>.
+
+          <span>
+            This site is licensed under a{" "}
+            <a
+              rel="license"
+              href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+            >
+              Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+            </a>.
+          </span>
         </p>
 
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
+
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
